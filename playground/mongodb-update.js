38efc3643d7c18718689817ea79001750e53a4c4
garfield <cmgarfield@gmail.com>
2017-11-08 22:49:05 +0800

    use findOneAndUpdate() to update a doc, remember to use update operator such as , and set returnOriginal to false to get the updated one

---
+++ playground/mongodb-update.js
@@ -0,0 +1,23 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+	if (err) {
+		return console.log('Unable to connect to MongoDB server');
+	}
+	console.log('Connected to MongoDB server');
+
+	db.collection('Todos').findOneAndUpdate({
+		_id : new ObjectID('5a02e906e1019d02a1257a5b')
+	}, {
+		$set : {
+			completed: true
+		}
+	}, {
+		returnOriginal: false
+	}).then((result) => {
+		console.log(result);
+	});
+
+	// db.close();
+});
