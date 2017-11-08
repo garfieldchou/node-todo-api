b80807fe73e01bbe5d3c735632743368933dccdf
garfield <shaudai0402@hotmail.com>
2017-11-08 09:32:30 +0800

    destructure user object, pull off the name property, create a new variable and set it equal to whatever the value is

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,5 +1,9 @@
 const MongoClient = require('mongodb').MongoClient;
 
+var user = {name: 'Andrew', age: 25};
+var {name} = user;
+console.log(name);
+
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 	if (err) {
 		return console.log('Unable to connect to MongoDB server');
@@ -18,17 +22,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 	// });
 
 	// Insert new doc into Users (name, age, location)
-	db.collection('Users').insertOne({
-		name: 'Andrew',
-		age: 25,
-		location: 'Philadelphia'
-	}, (err, result) => {
-		if(err) {
-			return console.log('Unable to insert user', err);
-		}
+	// db.collection('Users').insertOne({
+	// 	name: 'Andrew',
+	// 	age: 25,
+	// 	location: 'Philadelphia'
+	// }, (err, result) => {
+	// 	if(err) {
+	// 		return console.log('Unable to insert user', err);
+	// 	}
 
-		console.log(result.ops[0]._id.getTimestamp());
-	});
+	// 	console.log(result.ops[0]._id.getTimestamp());
+	// });
 
 	db.close();
 });
