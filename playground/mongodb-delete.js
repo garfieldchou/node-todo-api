a86327a3baf9ec8ceb42e67f2f4803e5265cfe43
garfield <cmgarfield@gmail.com>
2017-11-08 22:19:18 +0800

    use findOneAndDelete() to delete the one with specific id

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -22,13 +22,13 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 	// 	console.log(result);
 	// });
 
-	db.collection('Users').deleteMany({name: 'Andrew'});
+	// db.collection('Users').deleteMany({name: 'Andrew'});
 
-	// db.collection('Users').findOneAndDelete({
-	// 	_id: new ObjectID('5a02f4709486fc030acce49d')
-	// }).then((results) => {
-	// 	console.log(JSON.stringify(results, undefined, 2));
-	// });
+	db.collection('Users').findOneAndDelete({
+		_id: new ObjectID('5a02f4709486fc030acce49d')
+	}).then((results) => {
+		console.log(JSON.stringify(results, undefined, 2));
+	});
 
 	// db.close();
 });
