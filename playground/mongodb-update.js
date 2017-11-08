79cc2797f0326e94d320f90aceabdf5d3fcd2bde
garfield <cmgarfield@gmail.com>
2017-11-09 06:35:43 +0800

    update the doc by set and inc mongo update operators

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -7,17 +7,31 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 	}
 	console.log('Connected to MongoDB server');
 
-	db.collection('Todos').findOneAndUpdate({
-		_id : new ObjectID('5a02e906e1019d02a1257a5b')
+	// db.collection('Todos').findOneAndUpdate({
+	// 	_id : new ObjectID('5a02e906e1019d02a1257a5b')
+	// }, {
+	// 	$set : {
+	// 		completed: true
+	// 	}
+	// }, {
+	// 	returnOriginal: false
+	// }).then((result) => {
+	// 	console.log(result);
+	// });
+
+	db.collection('Users').findOneAndUpdate({
+		_id: new ObjectID('5a02f46d74989f03094e4ebd')
 	}, {
 		$set : {
-			completed: true
+			name: 'Andrew'
+		},
+		$inc : {
+			age: 1
 		}
 	}, {
 		returnOriginal: false
 	}).then((result) => {
 		console.log(result);
 	});
-
 	// db.close();
 });
