223e50c1bede3c12aa747a986c8bff30b697c198
garfield <shaudai0402@hotmail.com>
2017-11-08 17:07:18 +0800

    deleteOne() deletes the first item it sees that matches the criteria and then it stops
    result: { n: 1, ok: 1 },

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -8,11 +8,14 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 	console.log('Connected to MongoDB server');
 
 	// deleteMany
-	db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
-		console.log(result);
-	});
+	// db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+	// 	console.log(result);
+	// });
 
 	// deleteOne
+	db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+		console.log(result);
+	});
 
 	// findOneAndDelete
 
