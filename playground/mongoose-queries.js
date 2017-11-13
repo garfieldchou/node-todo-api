e2d2ef25267e45b3f4a5e62b20860d385b8ff67f
garfield <shaudai0402@hotmail.com>
2017-11-13 09:00:28 +0800

    catch the 'CastError: Cast to ObjectId failed for value...' if the id is invalid

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,23 +1,23 @@
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 
-var id = '6a08ebd99cd66c98189f25a9';
+var id = '5a08ebd99cd66c98189f25a911';
 
-Todo.find({
-    _id: id
-}).then((todos) => {
-    console.log('Todos', todos);
-});
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
 
-Todo.findOne({
-    _id: id
-}).then((todo) => {
-    console.log('Todo', todo);
-});
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo', todo);
+// });
 
 Todo.findById(id).then((todo) => {
 	if(!todo) {
 		return console.log('Id not found');
 	}
     console.log('Todo By Id', todo);
-});
+}).catch((e) => console.log(e));
