a9fd3c6af85aa72fdabc8541907b9a06f25c549c
garfield <shaudai0402@hotmail.com>
2017-11-13 17:32:42 +0800

    use findById() to get the query result and add error handling

--- server/server.js
+++ server/server.js
@@ -34,14 +34,14 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
 
-    console.log(id);
+    // console.log(id);
 
     // Validate id using isValid
         // 404 - send back empty body
     if (!ObjectID.isValid(id)) {
         res.status(404).send({});
     }
-    res.status(200).send({id});
+    // res.status(200).send({id});
 
     // findById
         // success
@@ -49,6 +49,12 @@ app.get('/todos/:id', (req, res) => {
             // if no todo - send back 404 with empty body
         // error
             // 400 - and send empty body back
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send({});
+        }
+        res.status(200).send({todo});
+    }).catch((e) => res.status(400).send({}));
 });
 
 app.listen(3000, () => {
