2f1cceb5ea088fef251c5400635a30fd1c07f369
garfield <shaudai0402@hotmail.com>
2017-11-14 17:46:11 +0800

    check the value of body.completed and set body.completeAt accordingly

--- server/server.js
+++ server/server.js
@@ -71,7 +71,18 @@ app.delete('/todos/:id', (req, res) => {
 
 app.patch('/todos/:id', (req, res) => {
     var id = req.params.id;
-    var body = _.pick(res.body, ['text', 'completed']);
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
 });
 
 app.listen(port, () => {
