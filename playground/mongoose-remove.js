5e3a37b7ccc733a99d2d24b196e397e1cc81c797
garfield <shaudai0402@hotmail.com>
2017-11-14 14:09:09 +0800

    use findByIdAndRemove() to remove one specific item by id

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -4,6 +4,13 @@ const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
 
-Todo.remove({}).then((result) => {
-	console.log(result);
+// Todo.remove({}).then((result) => {
+// 	console.log(result);
+// });
+
+// Todo.findOneAndRemove
+// Todo.findByIdAndRemove
+
+Todo.findByIdAndRemove('5a0a87fb1a3e889c45133b52').then((todo) => {
+	console.log(todo);
 });
