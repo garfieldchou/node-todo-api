91da6079922b14bc696703356b7974141dccd085
garfield <shaudai0402@hotmail.com>
2017-11-14 16:41:53 +0800

    add two more test cases for DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -123,13 +123,21 @@ describe('DELETE /todos/:id', () => {
 			});
 	});
 
-	// it('should return 404 if todo not found', (done) => {
+	it('should return 404 if todo not found', (done) => {
+		var hexId = new ObjectID().toHexString();
 
-	// });
+		request(app)
+			.delete(`/todos/${hexId}`)
+			.expect(404)
+			.end(done);
+	});
 
-	// it('should return 404 if object id is invalid', (done) => {
-
-	// });
+	it('should return 404 if object id is invalid', (done) => {
+		request(app)
+			.delete('/todos/123abc')
+			.expect(404)
+			.end(done);
+	});
 });
 
 
