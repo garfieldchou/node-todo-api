4c1255e5d82fc1e47b698c6ecb2e14e1cbeb94cf
garfield <cmgarfield@gmail.com>
2017-11-20 22:54:18 +0800

    use jwt.verify() to decode received token
    token:
    eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTUxMTE4OTM5NH0.dTuxaP0S5R-svcARtzqWCbuVBg8oAq7SajBwBNmepZE
    
    decoded result:
    decoded { id: 10, iat: 1511189394 }

--- playground/hashing.js
+++ playground/hashing.js
@@ -8,6 +8,9 @@ var data = {
 var token = jwt.sign(data, '123abc'); 
 console.log(token);
 
+var decoded = jwt.verify(token, '123abc');
+console.log('decoded', decoded);
+
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 
