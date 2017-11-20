c45f8ee16b480a3ac7fb1974c9b4ae86e786bd7e
garfield <cmgarfield@gmail.com>
2017-11-20 22:18:32 +0800

    use salt while hashing and test if the data was changed or not

--- playground/hashing.js
+++ playground/hashing.js
@@ -5,3 +5,20 @@ var hash = SHA256(message).toString();
 
 console.log(`Message: ${message}`);
 console.log(`Hash: ${hash}`);
+
+var data = {
+    id: 4
+};
+var token = {
+    data,
+    hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+}
+
+
+var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+
+if (resultHash === token.hash) {
+    console.log('Data was not changed');
+} else {
+    console.log('Data was changed. Do not trust');
+}
