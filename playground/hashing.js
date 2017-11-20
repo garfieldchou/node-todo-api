61446d380fca9d2336a36bc0c8165012662357ff
garfield <cmgarfield@gmail.com>
2017-11-20 22:46:26 +0800

    use jwt.sign() to create a token with salt
    https://jwt.io/

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,26 +1,34 @@
 const {SHA256} = require('crypto-js');
-
-var message = 'I am user number 3';
-var hash = SHA256(message).toString();
-
-console.log(`Message: ${message}`);
-console.log(`Hash: ${hash}`);
+const jwt = require('jsonwebtoken');
 
 var data = {
-    id: 4
+    id: 10
 };
-var token = {
-    data,
-    hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-}
 
-token.data.id = 5;
-token.hash = SHA256(JSON.stringify(token.data)).toString();
+var token = jwt.sign(data, '123abc'); 
+console.log(token);
 
-var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+// var message = 'I am user number 3';
+// var hash = SHA256(message).toString();
 
-if (resultHash === token.hash) {
-    console.log('Data was not changed');
-} else {
-    console.log('Data was changed. Do not trust');
-}
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+
+// var data = {
+//     id: 4
+// };
+// var token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// }
+
+// token.data.id = 5;
+// token.hash = SHA256(JSON.stringify(token.data)).toString();
+
+// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+
+// if (resultHash === token.hash) {
+//     console.log('Data was not changed');
+// } else {
+//     console.log('Data was changed. Do not trust');
+// }
