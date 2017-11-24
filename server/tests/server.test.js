c1878a48bd6cc8a041e9f2380dc69b61dddc7740
garfield <shaudai0402@hotmail.com>
2017-11-24 09:09:03 +0800

    use .set(k, v) to set a header for request
    k: header name
    v: header value

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -173,7 +173,9 @@ describe('PATCH /todos/:id', () => {
 
 describe('GET /users/me', () => {
 	it('should return user if authenticated', (done) => {
-
+		request(app)
+			.get('/users/me')
+			.set('x-auth', users[0].tokens[0].token)
 	});
 
 	it('should return 401 if not authenticated', (done) => {
