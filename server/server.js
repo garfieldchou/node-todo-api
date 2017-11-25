66fd04817a666e9b2f30dcb6fc9eec9143043c7e
garfield <cmgarfield@gmail.com>
2017-11-25 17:11:26 +0800

    send the user back if login succeeds or send status code 400 if not

--- server/server.js
+++ server/server.js
@@ -119,9 +119,9 @@ app.post('/users/login', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
 
     User.findByCredentials(body.email, body.password).then((user) => {
-
+        res.send(user);
     }).catch((e) => {
-
+        res.status(400).send();
     });
 });
 
