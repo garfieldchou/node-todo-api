944485c30481a84d3f67d1c3d838429f859ebf98
garfield <cmgarfield@gmail.com>
2017-11-29 20:19:17 +0800

    fix the test case error: keep the expected length as 1 instead of 0

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -293,7 +293,7 @@ describe('POST /users/login', () => {
 				 }
 
 				 User.findById(users[1]._id).then((user) => {
-					 expect(user.tokens.length).toBe(0);
+					 expect(user.tokens.length).toBe(1);
 					 done();
 				 }).catch((e) => done(e));
 			 });
