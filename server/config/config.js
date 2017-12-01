0f7ec2f40e26f4e9c6ecb6c05b70183490c53176
garfield <cmgarfield@gmail.com>
2017-12-01 20:08:03 +0800

    use forEach() to loop over the array of keys and set process.env properties accordingly

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,9 @@ if (env === 'development' || env === 'test') {
     var config = require('./config.json');
     var envConfig = config[env];
 
-    console.log(Object.keys(envConfig));
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
 }
 
 // if (env === 'development') {
