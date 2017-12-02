01fd184d5c928a01414d957be73267cf71426818
garfield <cmgarfield@gmail.com>
2017-12-02 15:11:35 +0800

    toNotExist() -> toBeFalsy(); toExist() -> toBeTruthy()

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -124,7 +124,7 @@ describe('DELETE /todos/:id', () => {
 				}
 
 				Todo.findById(hexId).then((todo) => {
-					expect(todo).toNotExist();
+					expect(todo).toBeFalsy();
 					done();
 				}).catch((e) => done(e));
 			});
@@ -143,7 +143,7 @@ describe('DELETE /todos/:id', () => {
 				}
 
 				Todo.findById(hexId).then((todo) => {
-					expect(todo).toExist();
+					expect(todo).toBeTruthy();
 					done();
 				}).catch((e) => done(e));
 			});
@@ -219,7 +219,7 @@ describe('PATCH /todos/:id', () => {
 			.expect((res) => {
 				expect(res.body.todo.text).toBe(text)
 				expect(res.body.todo.completed).toBe(false)
-				expect(res.body.todo.completedAt).toNotExist();
+				expect(res.body.todo.completedAt).toBeFalsy();
 			})
 			.end(done);
 	});
@@ -259,8 +259,8 @@ describe('POST /users', () => {
 			.send({email, password})
 			.expect(200)
 			.expect((res) => {
-				expect(res.headers['x-auth']).toExist();
-				expect(res.body._id).toExist();
+				expect(res.headers['x-auth']).toBeTruthy();
+				expect(res.body._id).toBeTruthy();
 				expect(res.body.email).toBe(email);
 			})
 			.end((err) => {
@@ -269,7 +269,7 @@ describe('POST /users', () => {
 				}
 
 				User.findOne({email}).then((user) => {
-					expect(user).toExist();
+					expect(user).toBeTruthy();
 					expect(user.password).toNotBe(password);
 					done();
 				}).catch((e) => done(e));
@@ -309,7 +309,7 @@ describe('POST /users/login', () => {
 			 })
 			 .expect(200)
 			 .expect((res) => {
-				 expect(res.headers['x-auth']).toExist();
+				 expect(res.headers['x-auth']).toBeTruthy();
 			 })
 			 .end((err, res) => {
 				 if (err) {
@@ -335,7 +335,7 @@ describe('POST /users/login', () => {
 			 })
 			 .expect(400)
 			 .expect((res) => {
-				 expect(res.headers['x-auth']).toNotExist();
+				 expect(res.headers['x-auth']).toBeFalsy();
 			 })
 			 .end((err, res) => {
 				 if (err) {
