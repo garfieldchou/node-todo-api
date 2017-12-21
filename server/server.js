0711b74d63bf7346ae6060f767989933780d1bce
garfield <cmgarfield@gmail.com>
2017-12-21 10:13:56 +0800

    make two more routes as async and use try-catch block to handle the errors
    don't forget to use 'npm test' after modification to make sure that it works

--- server/server.js
+++ server/server.js
@@ -60,25 +60,26 @@ app.get('/todos/:id', authenticate, (req, res) => {
     });
 });
 
-app.delete('/todos/:id', authenticate, (req, res) => {
-    var id = req.params.id;
+app.delete('/todos/:id', authenticate, async (req, res) => {
+    const id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
 
-    Todo.findOneAndRemove({
-        _id: id,
-        _creator: req.user._id
-    }).then((todo) => {
+    try {
+        const todo = await Todo.findOneAndRemove({
+            _id: id,
+            _creator: req.user._id
+        });
         if (!todo) {
             return res.status(404).send();
         }
 
         res.send({todo});
-    }).catch((e) => {
+    } catch(e) {
         res.status(400).send();
-    });
+    }
 });
 
 app.patch('/todos/:id', authenticate, (req, res) => {
@@ -106,17 +107,16 @@ app.patch('/todos/:id', authenticate, (req, res) => {
     });
 });
 
-app.post('/users', (req, res) => {
-    var body = _.pick(req.body, ['email', 'password']);
-    var user = new User(body);
-
-    user.save().then(() => {
-        return user.generateAuthToken();
-    }).then((token) => {
+app.post('/users', async (req, res) => {
+    try{
+        const body = _.pick(req.body, ['email', 'password']);
+        const user = new User(body);
+        await user.save();
+        const token = user.generateAuthToken();
         res.header('x-auth', token).send(user);
-    }).catch((e) => {
+    } catch(e) {
         res.status(400).send(e);
-    });
+    }
 });
 
 app.get('/users/me', authenticate, (req, res) => {
